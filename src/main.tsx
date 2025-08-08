b063bdc2645d818866c6c87784137c49e09a07fb
Rakib Hossain <hossainrokib55@gmail.com>
2025-08-08 07:38:41 +0600

    register,login

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import { ThemeProvider } from "./providers/theme.provider";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./redux/store";
+import { Toaster } from "sonner";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ReduxProvider store={store}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+          <Toaster />
+      </ThemeProvider>
+    </ReduxProvider>
   </StrictMode>
 );
